d1ba3e8563ab4fbe0ff97adc9c753ed5eb0d74f8
SlideMovies <87663026+SlideMovies@users.noreply.github.com>
2023-06-01 21:30:25 +0500

    Create torrent.php

---
+++ torrent.php
@@ -0,0 +1,13 @@
+<?php
+          header("Content-type: application/json");
+          echo "{";
+          $dir = 'sqlite:rarbg_db.sqlite';
+          $dbh  = new PDO($dir) or die("cannot open the database");
+          $query =  "SELECT * FROM 'items' WHERE ext_id!='$_GET[id]' LIMIT 0,1";
+          $results = $dbh->query($query);
+          foreach ($results as $row) {
+            echo "{\"hash\":\"$row[1]\", \"name\": \"$row[2]\", \"date\": \"$row[3]\", \"cat\": \"$row[4]\", \"size\": $row[5], \"ext_id\": \"$row[6]\", \"imdb_id\": \"$row[7]\"}";
+          }
+          $dbh = null;
+          echo "}";
+?>
