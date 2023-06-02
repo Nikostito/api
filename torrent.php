e32c5064a4a234c1dc2e5efb2e0c15597bb1d144
SlideMovies <87663026+SlideMovies@users.noreply.github.com>
2023-06-02 11:24:30 +0500

    Fix SQL Injection

--- torrent.php
+++ torrent.php
@@ -2,7 +2,7 @@
           header("Content-type: application/json");
           $dir = 'sqlite:rarbg_db.sqlite';
           $dbh  = new PDO($dir) or die("cannot open the database");
-          $query =  "SELECT * FROM 'items' WHERE ext_id='$_GET[id]' LIMIT 0,1";
+          $query =  "SELECT * FROM 'items' WHERE ext_id='".str_replace("'", "\"", $_GET[id])."' LIMIT 0,1";
           $results = $dbh->query($query);
           foreach ($results as $row) {
             echo "{\"hash\":\"$row[1]\", \"name\": \"$row[2]\", \"date\": \"$row[3]\", \"cat\": \"$row[4]\", \"size\": $row[5], \"ext_id\": \"$row[6]\", \"imdb_id\": \"$row[7]\"}";
