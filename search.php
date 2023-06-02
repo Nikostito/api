bf89f8c18523b1e600c3009637219b1a7f61566b
Sex Habibi <90955030+HankMooody@users.noreply.github.com>
2023-06-02 23:50:02 +0530

    Update search.php

--- search.php
+++ search.php
@@ -1,22 +1,46 @@
 <?php
-          header("Content-type: application/json");
+        require "config.php";
+        header("Content-type: application/json");
+        if (!isset($_GET['key'])) {
+            die("API key missing."); 
+        }
+        if (!in_array($_GET['key'], $api_keys)) {
+            die("API key invalid");   
+        }
+        header("Content-type: application/json");
+        $dir = 'sqlite:rarbg_db.sqlite';
+        $dbh  = new PDO($dir) or die("cannot open the database");
+        $q = $_GET['query'];
+        //$query =  $dbh->prepare("SELECT * FROM 'items' WHERE cat != 'xxx' AND title LIKE :like ORDER BY dt DESC");
+        $query = "SELECT * FROM 'items' WHERE cat != 'xxx' AND title LIKE ";
+        if (strpos($q, ' ') !== false) {
+            $parts = explode(' ', $q);
+            $like = '';
+            foreach ($parts as $word) {
+                $query.=" '%$word%' OR ";
+                }
+            $finalquery = rtrim($query, " OR");
+            } 
+        else {
+        $query.= '%'. $q . '%';
+        } 
+        $data= $dbh->query($finalquery);
+        $results = $data->fetchAll(PDO::FETCH_ASSOC);
+        $total = count($results);
+        if ($results) {
+            $total = count($results);
+        
+            $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+            $results_per_page = 20;
+            $offset = ($page - 1) * $results_per_page;
+            $limited_results = array_slice($results, $offset, $results_per_page);
 
-          $dir = 'sqlite:rarbg_db.sqlite';
-          $dbh  = new PDO($dir) or die("cannot open the database");
-
-          $query =  $dbh->prepare("SELECT * FROM 'items' WHERE cat != 'xxx' AND title LIKE '%".str_replace("'", "\"", $_GET['query'])."%' ORDER BY dt DESC");
-          $query->execute();
-          $results = $query->fetchAll(PDO::FETCH_ASSOC);
-          $total = count($results);
-
-          $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
-          $results_per_page = 20;
-          $offset = ($page - 1) * $results_per_page;
-          $limited_results = array_slice($results, $offset, $results_per_page);
-
-          header('Content-type: application/json');
-          echo json_encode(array('results' => $limited_results, 'count' => $total,'pages' => ceil($total/$results_per_page)));
-
-          $dbh = null;
+            header('Content-type: application/json');
+            echo json_encode(array('data' => $limited_results, 'count' => $total,'pages' => ceil($total/$results_per_page),'query'=>$q));
+        }
+        else {
+            echo "Error: " . $q . "<br>" . $dbh->errorInfo()[2];
+        }
+        $dbh = null;
 
 ?> 
