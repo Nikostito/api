25fd22a879bfec9d37b1f85940f6594e85c2dc07
SlideMovies <87663026+SlideMovies@users.noreply.github.com>
2023-06-03 11:55:39 +0500

    Fix SQL injection vulnerability

--- torrent.php
+++ torrent.php
@@ -10,9 +10,9 @@
           header("Content-type: application/json");
           $dir = 'sqlite:rarbg_db.sqlite';
           $dbh  = new PDO($dir) or die("cannot open the database");
-          $query =  "SELECT * FROM 'items' WHERE ext_id='".str_replace("'", "\"", $_GET['id'])."' LIMIT 0,1";
-          $results = $dbh->query($query);
-          foreach ($results as $row) {
+          $stmt = $dbh->prepare("SELECT * FROM 'items' WHERE ext_id=:id LIMIT 0,1");
+          $stmt->execute([ 'id' => $_GET['id'] ]);
+          foreach ($stmt as $row) {
             echo "{\"hash\":\"$row[1]\", \"name\": \"$row[2]\", \"date\": \"$row[3]\", \"cat\": \"$row[4]\", \"size\": $row[5], \"ext_id\": \"$row[6]\", \"imdb_id\": \"$row[7]\"}";
           }
           $dbh = null;
